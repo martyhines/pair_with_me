0ca3534ef4106ce3c6b1dd41b245048e00549672
Marty Hines <mhines76@gmail.com>
2013-04-05 20:13:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pairwithme::Application.config.secret_token = '45a8039a1376f755dd2d45b399acb81d8cec08b3c4e81a9099697680e7aa3a95305d87218ae4c30ec4777b13db21507f49a5cba44a13c0c57a9c79f94eb520ab'
