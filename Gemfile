34b8e6b96c64e6ef06967dede1808c906019cb00
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2013-08-04 19:30:02 +0000

    refactored tweets controller into index and show actions with @jeremy_w_rowe

--- Gemfile
+++ Gemfile
@@ -19,16 +19,16 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
 
-group :production do
-  gem 'pg', '0.12.2'
-end
+#group :production do
+  #gem 'pg', '0.12.2'
+#end
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,63 +1,33 @@
 class TweetsController < ApplicationController
-  before_filter :regex
+  before_filter :regex, :set_technologies 
   require "rubygems"
   require "twitter"
-  Twitter.configure do |config|
-    config.consumer_key = 'SIA2IocBpvIjoe4AQ1wg'
-    config.consumer_secret =  'UcablPTEeywWAxzKg5MP1VUFFoPppPaR1HBVQXCJ8g'
-    config.oauth_token = '252830982-Sga3qjleThLPTEtJYD8hH1Vko046XABsUZ3X3RxM'
-    config.oauth_token_secret = 'w5lPpTOovhhMJqzEQ1zQM44Ujo8FDrnfvEDCYTabk'
-  end
 
   def index
-    client = Twitter::Client.new
-    @tweets = client.search('pairwithme', :count => 100).statuses
+    @tweets = TWITTER_CLIENT.search("pairwithme", :count => 100).statuses
     @search = "all"
   end
-  def api
-    client = Twitter::Client.new
-    @tweets = client.search(params[:q], :count => 100)
 
-    render :layout => nil, :format => 'json'
-  end
-  def ember
-    client = Twitter::Client.new
-    @search = "Ember"
-    @tweets = client.search('pairwithme+ember', :count => 100).statuses
-  end
-  def javascript
-    client = Twitter::Client.new
-    @search = "JavaScript"
-    @tweets = client.search('pairwithme+javascript', :count => 100).statuses
-  end
-  def objectivec
-    client = Twitter::Client.new
-    @search = "Objective-C"
-    @tweets = client.search('pairwithme+objective', :count => 100).statuses
-  end
-  def opensource
-    client = Twitter::Client.new
-    @search = "Open Source"
-    @tweets = client.search('pairwithme+open%20source', :count => 100).statuses
-  end
-  def python
-    client = Twitter::Client.new
-    @search = "Python"
-    @tweets = client.search('pairwithme+python', :count => 100).statuses
-  end
-  def ruby
-    client = Twitter::Client.new
-    @search = "Ruby"
-    @tweets = client.search('pairwithme+ruby', :count => 100).statuses
-  end
-  def search
-    client = Twitter::Client.new
-    @tweets = client.search('pairwithme+'+ params[:search], :count => 100).statuses
+  def show
     @search = params[:search]
+    @tweets = TWITTER_CLIENT.search("pairwithme+#{URI.escape(@search)}", :count => 100).statuses
   end
 
+  private
   def regex
     @regex = Regexp.new(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)
+  end
+
+  def set_technologies
+    @technologies = {
+      csharp:     "C#",
+      ember:      "Ember",
+      javascript: "JavaScript",
+      lisp:       "Lisp",
+      objectivec: "Objective-C",
+      opensource: "Open Source",
+      ruby:       "Ruby",
+    }
   end
 end
 
