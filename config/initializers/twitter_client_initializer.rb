852943b456e30fe62986e84d1ce367edc2f8923c
martyhines <mhines76@gmail.com>
2013-08-06 17:35:18 -0400

    fix ENV variables typo

--- config/initializers/twitter_client_initializer.rb
+++ config/initializers/twitter_client_initializer.rb
@@ -1,9 +1,8 @@
 Twitter.configure do |config|
-  config.consumer_key       = ENV [CONSUMER_KEY]
-  config.consumer_secret    = ENV [CONSUMER_SECRET]
-  config.oauth_token        = ENV [OAUTH_TOKEN]
-  config.oauth_token_secret = ENV [OAUTH_TOKEN_SECRET]
-  #make sure to set environment variable on heroku
+  config.consumer_key       = ENV[CONSUMER_KEY]
+  config.consumer_secret    = ENV[CONSUMER_SECRET]
+  config.oauth_token        = ENV[OAUTH_TOKEN]
+  config.oauth_token_secret = ENV[OAUTH_TOKEN_SECRET]
 end
 
 TWITTER_CLIENT = Twitter::Client.new
