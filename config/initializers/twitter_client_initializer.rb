c759baf8d40fc3115c11657b808c259813549b77
martyhines <mhines76@gmail.com>
2013-08-06 17:42:35 -0400

    fix ENV variables typo

--- config/initializers/twitter_client_initializer.rb
+++ config/initializers/twitter_client_initializer.rb
@@ -1,8 +1,8 @@
 Twitter.configure do |config|
-  config.consumer_key       = ENV[CONSUMER_KEY]
-  config.consumer_secret    = ENV[CONSUMER_SECRET]
-  config.oauth_token        = ENV[OAUTH_TOKEN]
-  config.oauth_token_secret = ENV[OAUTH_TOKEN_SECRET]
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
 end
 
 TWITTER_CLIENT = Twitter::Client.new
