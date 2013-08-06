a641d9e55f8e257ab2d4eda593efac1751da7569
martyhines <mhines76@gmail.com>
2013-08-05 23:11:19 -0400

    brought it in rails_autolink

--- Gemfile
+++ Gemfile
@@ -4,13 +4,14 @@ gem 'rails', '3.2.7'
 
 gem 'twitter'
 
+gem 'rails_autolink'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development do
   gem 'sqlite3', '1.3.7'
 end
-
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -29,18 +30,3 @@ gem 'jquery-rails'
 group :production do
   gem 'pg', '0.12.2'
 end
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,4 @@
 class TweetsController < ApplicationController
-  before_filter :regex
   before_filter :set_technologies 
   require "rubygems"
   require "twitter"
@@ -9,25 +8,15 @@ class TweetsController < ApplicationController
     @search = "all"
   end
 
-  def search
-    # Todo: remove when search.js is working (from routes as well)
-    @search = params[:search]
-    @tweets = TWITTER_CLIENT.search("pairwithme+#{URI.escape(@search)}", :count => 100).statuses
-    render :show
-  end
-
   def show
     @search = params[:search]
     @tweets = TWITTER_CLIENT.search("pairwithme+#{URI.escape(@search)}", :count => 100).statuses
   end
 
   private
-  def regex
-    # Regexp.new(/@\w+/)
-    #
-    # look into rails_autolink
-    # autolink(content).html_safe
-    @regex = Regexp.new(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)
+  def username_link
+    # to find twitter handle
+    #Regexp.new(/@\w+/)
   end
 
   def set_technologies
@@ -38,6 +27,7 @@ class TweetsController < ApplicationController
       lisp:       "Lisp",
       objectivec: "Objective-C",
       opensource: "Open Source",
+      python:     "Python",
       ruby:       "Ruby",
     }
   end
